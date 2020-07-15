ed417c3dd4ab3561ae8918e5cc27b0228fdcb991
Maximilian Anton Metz <metzmax@web.de>
2020-07-15 11:01:39 +0200

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by just_mexstar on 15.07.20.
+//  Copyright © 2020 Maximilian Anton Metz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
